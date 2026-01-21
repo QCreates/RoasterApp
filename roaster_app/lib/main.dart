454977e87f055d2c56cba9065239f4815e1c0f7c
QCreates <qasem458@gmail.com>
2026-01-20 23:08:58 -0600

    Initial Commit

---
+++ roaster_app/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const MainTabs(),
+    );
+  }
+}
+
+class MainTabs extends StatelessWidget {
+  const MainTabs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter App'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'Tab 1'),
+              Tab(text: 'Tab 2'),
+              Tab(text: 'Tab 3'),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            Center(child: Text('Tab 1')),
+            Center(child: Text('Tab 2')),
+            Center(child: Text('Tab 3')),
+          ],
+        ),
+      ),
+    );
+  }
+}
